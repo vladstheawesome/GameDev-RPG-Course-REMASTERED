4dfe465611407ca8d68c790288ebbcc37e3b8e34
vladstheawesome <vmabhena@gmail.com>
2019-04-07 11:17:16 +0100

    Section 04 - 25. Raycasting For Components

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,41 +1,42 @@
-﻿//using UnityEngine;
-//using RPG.Movement;
+﻿using UnityEngine;
+using RPG.Movement;
 
-//namespace RPG.Combat
-//{
-//    public class Fighter : MonoBehaviour
-//    {
-//        [SerializeField] float weaponRange = 2f;
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        //        [SerializeField] float weaponRange = 2f;
 
-//        Transform target;
+        //        Transform target;
 
-//        private void Update()
-//        {
-//            if (target == null) return;
+        //        private void Update()
+        //        {
+        //            if (target == null) return;
 
-//            if (target != null && !GetIsInRange())
-//            {
-//                GetComponent<Mover>().MoveTo(target.position);
-//            }
-//            else
-//            {
-//                GetComponent<Mover>().Stop();
-//            }
-//        }
+        //            if (target != null && !GetIsInRange())
+        //            {
+        //                GetComponent<Mover>().MoveTo(target.position);
+        //            }
+        //            else
+        //            {
+        //                GetComponent<Mover>().Stop();
+        //            }
+        //        }
 
-//        private bool GetIsInRange()
-//        {
-//            return Vector3.Distance(transform.position, target.position) < weaponRange;
-//        }
+        //        private bool GetIsInRange()
+        //        {
+        //            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        //        }
 
-//        public void Attack(CombatTarget combatTarget)
-//        {
-//            target = combatTarget.transform;
-//        }
+        public void Attack(CombatTarget target)
+        {
+            //target = combatTarget.transform;
+            print("take that you enemy!!");
+        }
 
-//        public void Cancel()
-//        {
-//            target = null;
-//        }
-//    }
-//}
+        //        public void Cancel()
+        //        {
+        //            target = null;
+        //        }
+    }
+}
