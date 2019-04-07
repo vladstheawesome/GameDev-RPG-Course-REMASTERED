b062b574d71a7f01ae8d3fb69f62ad478f3814ae
vladstheawesome <vmabhena@gmail.com>
2019-04-07 13:22:11 +0100

    Section 04 - 27. Move Within Range

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -5,32 +5,26 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        //        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float weaponRange = 2f;
 
-        //        Transform target;
+        Transform target;
 
-        //        private void Update()
-        //        {
-        //            if (target == null) return;
-
-        //            if (target != null && !GetIsInRange())
-        //            {
-        //                GetComponent<Mover>().MoveTo(target.position);
-        //            }
-        //            else
-        //            {
-        //                GetComponent<Mover>().Stop();
-        //            }
-        //        }
-
-        //        private bool GetIsInRange()
-        //        {
-        //            return Vector3.Distance(transform.position, target.position) < weaponRange;
-        //        }
-
-        public void Attack(CombatTarget target)
+        private void Update()
         {
-            //target = combatTarget.transform;
+            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+            if (target != null && !isInRange)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
             print("take that you enemy!!");
         }
 
