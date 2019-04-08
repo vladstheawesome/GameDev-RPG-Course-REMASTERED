41c71fe6974975cc6d397b41a641e2a1903d539c
vladstheawesome <vmabhena@gmail.com>
2019-04-08 21:11:09 +0100

    Section 04 - 32. Trigger Animation In Code

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -25,7 +25,6 @@ namespace RPG.Movement
         public void StartMoveAction(Vector3 destination)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            //GetComponent<Fighter>().Cancel();
             MoveTo(destination);
         }
 
