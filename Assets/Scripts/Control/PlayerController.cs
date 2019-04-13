55f280a103f06fb5a1aa11acac5a7ac6c8aae41c
vladstheawesome <vmabhena@gmail.com>
2019-04-13 14:19:24 +0100

    Section 04 - 41. Ignore Dead Enemies

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -20,7 +20,11 @@ namespace RPG.Control
             foreach (RaycastHit hit in hits)
             {
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if (target == null) continue;
+                //if (target == null) continue;
+                if(!GetComponent<Fighter>().CanAttack(target))
+                {
+                    continue;
+                }
 
                 if (Input.GetMouseButtonDown(0))
                 {
