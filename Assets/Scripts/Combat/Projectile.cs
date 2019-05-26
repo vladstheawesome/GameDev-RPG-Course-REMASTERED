2f61a95aafe697786dcd0d40cbe5500bb3c008c4
vladstheawesome <vmabhena@gmail.com>
2019-05-26 14:57:37 +0100

    Section 09 - 105. Damage From Projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,8 +9,8 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1;
-
         Health target = null;
+        float damage = 0;
 
         void Update()
         {
@@ -20,9 +20,10 @@ namespace RPG.Combat
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
-        public void SetTarget(Health target)
+        public void SetTarget(Health target, float damage)
         {
             this.target = target;
+            this.damage = damage;
         }
 
         private Vector3 GetAimLocation()
@@ -33,6 +34,13 @@ namespace RPG.Combat
                 return target.transform.position;
             }
             return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() != target) return;
+            target.TakeDamage(damage);
+            Destroy(gameObject);
         }
     }
 }
