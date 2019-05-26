8e84faf23d3afb4cbd535d262ec700e23a36bfe5
vladstheawesome <vmabhena@gmail.com>
2019-05-26 14:30:28 +0100

    Section 09 - 104. Shoot Projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPG.Core;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,10 @@ namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] Transform target = null;
         [SerializeField] float speed = 1;
 
-        // Update is called once per frame
+        Health target = null;
+
         void Update()
         {
             if (target == null) return;
@@ -19,14 +20,19 @@ namespace RPG.Combat
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
+        public void SetTarget(Health target)
+        {
+            this.target = target;
+        }
+
         private Vector3 GetAimLocation()
         {
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
             if (target == null)
             {
-                return target.position;
+                return target.transform.position;
             }
-            return target.position + Vector3.up * targetCapsule.height / 2;
+            return target.transform.position + Vector3.up * targetCapsule.height / 2;
         }
     }
 }
