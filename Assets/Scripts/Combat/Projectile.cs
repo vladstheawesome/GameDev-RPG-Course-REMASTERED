cff17ed47a8a525d9dd7b3e2ed81fc38a90bd5f8
vladstheawesome <vmabhena@gmail.com>
2019-05-28 22:07:00 +0100

    Section 09 - 112. Destory The Unwanted

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -11,6 +11,10 @@ namespace RPG.Combat
         [SerializeField] float speed = 1;
         [SerializeField] bool isHoming = true;
         [SerializeField] GameObject hitEffect = null;
+        [SerializeField] float maxLifeTime = 10f;
+        [SerializeField] GameObject[] destroyOnHit = null;
+        [SerializeField] float lifeAfterImpact = 2f;
+
         Health target = null;
         float damage = 0;
 
@@ -33,6 +37,8 @@ namespace RPG.Combat
         {
             this.target = target;
             this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
         }
 
         private Vector3 GetAimLocation()
@@ -51,12 +57,19 @@ namespace RPG.Combat
             if (target.IsDead()) return;
             target.TakeDamage(damage);
 
+            speed = 0;
+
             if(hitEffect != null)
             {
                 Instantiate(hitEffect, GetAimLocation(), transform.rotation);
             }
 
-            Destroy(gameObject);
+            foreach(GameObject toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, lifeAfterImpact);
         }
     }
 }
