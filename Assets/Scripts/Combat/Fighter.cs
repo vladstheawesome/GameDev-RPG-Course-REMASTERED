45fbb8dfa9e8d8c04aec27a0b124f81a6b31cfca
vladstheawesome <vmabhena@gmail.com>
2019-06-04 23:09:23 +0100

    Section 09 - 115. Saving Weapon Choice

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -2,10 +2,11 @@
 using RPG.Movement;
 using RPG.Core;
 using System;
+using RPG.Saving;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour, IAction
+    public class Fighter : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] Transform rightHandTransform = null;
@@ -17,8 +18,11 @@ namespace RPG.Combat
         Weapon currentWeapon = null;
 
         private void Start()
-        {
-            EquipWeapon(defaultWeapon);
+        {        
+            if(currentWeapon == null)
+            {
+                EquipWeapon(defaultWeapon);
+            }            
         }
 
         private void Update()
@@ -112,6 +116,18 @@ namespace RPG.Combat
         {
             GetComponent<Animator>().ResetTrigger("attack");
             GetComponent<Animator>().SetTrigger("stopAttack");
+        }
+
+        public object CaptureState()
+        {
+            return currentWeapon.name;
+        }
+
+        public void RestoreState(object state)
+        {
+            string weaponName = (string)state;
+            Weapon weapon = Resources.Load<Weapon>(weaponName);
+            EquipWeapon(weapon);
         }
     }
 }
