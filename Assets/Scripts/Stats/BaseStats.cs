2e5d8f65f63d30126cdead96261966f91d52d1fc
vladstheawesome <vmabhena@gmail.com>
2019-06-19 13:57:04 +0100

    Section 10 - 132, Delegates and Actions For Levelling Up

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -11,11 +11,25 @@ namespace RPG.Stats
         [SerializeField] CharacterClass characterClass;
         [SerializeField] Progression progession = null;
 
-        private void Update()
+        int currentLevel = 0;
+
+        private void Start()
         {
-            if (gameObject.tag == "Player")
+            currentLevel = CalculateLevel();
+            Experience experience = GetComponent<Experience>();
+            if(experience != null)
             {
-                //print(GetLevel());
+                experience.onExperienceGained += UpdateLevel;
+            }
+        }
+
+        private void UpdateLevel()
+        {
+            int newLevel = CalculateLevel();
+            if(newLevel > currentLevel)
+            {
+                currentLevel = newLevel;
+                print("Levelled up!");
             }
         }
 
@@ -25,6 +39,15 @@ namespace RPG.Stats
         }
 
         public int GetLevel()
+        {
+            if (currentLevel < 1)
+            {
+                currentLevel = CalculateLevel();
+            }
+            return currentLevel;
+        }
+
+        public int CalculateLevel()
         {
             Experience experience = GetComponent<Experience>();
             if (experience == null) return startingLevel;
