8f9e9d3a5b941ed816b7e0f527e67bd20c7a9da2
vladstheawesome <vmabhena@gmail.com>
2019-11-23 14:29:34 +0000

    NPC Prefab Variant

---
+++ Assets/Scripts/Combat/NpcTarget.cs
@@ -0,0 +1,18 @@
+﻿using RPG.Control;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat
+{
+    public class NpcTarget : MonoBehaviour, IRaycastable
+    {
+        public CursorType GetCursorType()
+        {
+            return CursorType.NPC;
+        }
+
+        public bool HandleRaycast(PlayerController callingController)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
