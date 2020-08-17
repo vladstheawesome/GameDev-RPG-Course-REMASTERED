5acf0f4e5ca25decaeb01e48e4ae371210358c56
vladstheawesome <vmabhena@gmail.com>
2020-08-17 15:01:23 +0100

    Random Enemy Drops

---
+++ Assets/Scripts/Inventories/RandomDropper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Inventories
+{
+    public class RandomDropper : ItemDropper
+    {
+        // CONFIG DATA
+        [Tooltip("How far can the pickups be scattered from the dropper.")]
+        [SerializeField] float scatterDistance = 1;
+        [SerializeField] InventoryItem[] dropLibrary;
+        [SerializeField] int numberOfDrops = 2;
+
+        // CONSTANTS
+        const int ATTEMPTS = 30;
+
+        public void RandomDrop()
+        {
+            for (int i = 0; i < numberOfDrops; i++)
+            {
+                var item = dropLibrary[Random.Range(0, dropLibrary.Length)];
+                DropItem(item, 1);
+            }
+        }
+
+        protected override Vector3 GetDropLocation()
+        {
+            for (int i = 0; i < ATTEMPTS; i++)
+            {
+                Vector3 randomPoint = transform.position + Random.insideUnitSphere * scatterDistance;
+
+                // We need to make sure the dropped item lands on the nav mesh (ground) 
+                // and not up in air / cliffside
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(randomPoint, out hit, 0.1f, NavMesh.AllAreas))
+                {
+                    return hit.position;
+                }
+            }
+            return transform.position;
+        }
+    }
+}
